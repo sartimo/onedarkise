d93387286b4e4496365b794c7ca371898d01b5a9
Timo Sarkar <71646577+0xtimosarkar@users.noreply.github.com>
2023-05-16 12:38:40 +0200

    Delete src/cmd directory

--- src/cmd/constants.rs
+++
@@ -1,3 +0,0 @@
-const DefaultProjectConfigFilename: &str = "kaiju.json";
-const DefaultCompilationPlatform: &str = "compile";
-const TargetFlagDescription: &str = "target contract or directory to compile";
